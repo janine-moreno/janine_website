ad99646a0d0e45e068dd7867e3b3581a1f529179
janine-moreno <janinemoreno@users.noreply.github.com>
2021-03-12 17:04:45 +0800

    dot.com version 1
    version 1

---
+++ jsdocs/janinewebsite.js
@@ -0,0 +1,11 @@
+/*javascript for janine_website
+MADE BY: JANINE ASHLEY C. MORENO
+*/
+
+
+
+
+//to display every webname
+for (i=0; i>=0; i++){
+	document.getElementsByClassName("webname")[i].innerHTML = "dot.com";
+}
